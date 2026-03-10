2d235cda11c0a69b2e3cc0f619b977ebf4f90030
K9Patel <k9patel1@gmail.com>
2026-03-10 13:10:03 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/watchtower/backend/WatchtowerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.watchtower.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchtowerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchtowerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/watchtower/backend/WatchtowerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchtower.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchtowerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
